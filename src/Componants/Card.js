c09c67cfe0a7ea8781106c7b63a8efc2765e34fb
ankitpanchal534 <ankitpanchal534@gmail.com>
2022-04-23 18:56:06 +0530

    mastar

--- src/Componants/Card.js
+++ src/Componants/Card.js
@@ -1,15 +1,17 @@
 import React from 'react'
-
+import './Card.css'
 export default function Card(props) {
   return (
  
          <div className='card' >
+           
 <img src={props.productimage} className='product-img' />
 <a href='{#}' className='view-product'>VIEW DETAIL</a>
-<span>{props.name}</span>
+<span className='card-text'>{props.name}</span>
 {/* {props.count} */}
-<span>Rs:{props.price}</span>
-
+<br />
+<span className='card-text'>Rs:{props.price}</span>
+<span className='size'>Size:&nbsp;&nbsp;&nbsp;6&nbsp;8&nbsp;10&nbsp;12&nbsp;14&nbsp;16&nbsp;18</span>
         </div>
 
   )
--- src/Componants/Product.jsx
+++ src/Componants/Product.jsx
@@ -2,32 +2,39 @@ import React,{useState} from 'react'
 import Card from './Card'
 import './Product.css'
 export default function Product() {
-  const [productdata , setProductdata] = useState([])
   
+  const [productdata , setProductdata] = useState([])
  
-  function getData() {
+  const getData = ()=> {
     fetch(
       `https://pim.wforwoman.com/pim/pimresponse.php/?service=category&store=1&url_key=top-wear-kurtas&page=1&count=20&sort_by=&sort_dir=desc&filter=`
     )
       .then((res) => res.json())
       .then((data) => {
         // console.log(data)
-        setProductdata(data.result.products)
-        console.log(productdata)
-       
+        if(productdata==""){
+      setProductdata(data.result.products)
+
+      }
+        else{
+        // console.log(productdata)
+        console.log(0)  
+
+      }
       
       });
   }
-
-
+  getData()
+  
   return (
  
     <div className='product-list' >
-   <button onClick={getData} >Show Data</button>
+        {/* <button onClick={getData} >Show Data</button> */}
+   
 
      {/* {productdata?<Card count={productdata[0].result.count} />:""} */}
      
-{productdata.map((item,i)=> <Card name={item.name} productimage={item.image} price={item.price}/>)}
+{productdata.map((item,i)=> <Card name={item.name} productimage={item.image} price={item.price} key={i}/>)}
       
 
 {/* <li key={i}>{item.name}</li> */}
